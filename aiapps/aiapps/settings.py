321160f41ce6de5d26d0bf66c1640dc81c9ba0d9
Kohei Shoji <koheishoji1106@gmail.com>
2020-09-24 00:35:19 +0900

    FIX: type

--- aiapps/aiapps/settings.py
+++ aiapps/aiapps/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'vehicle.apps.VehicleConfig'
+    'vehicle.apps.VehicleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
